bd7e9612c4c4136ca60c7e96ecdd3d26ff536cb9
pgopasina <pgopasina@miraclesoft.com>
2024-04-15 19:48:00 +0530

    Added docker file

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hello From Prudhvi" }');
 });
 
 app.get('/will', function (req, res) {
     res.send('{ "response": "Hello World" }');
 });
+
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": "Great!, It works!" }');
 });
-app.listen(process.env.PORT || 3000);
+
+app.listen(port, () => {
+    console.log('App is running successfully on port: ' + port);
+});
+
 module.exports = app;
