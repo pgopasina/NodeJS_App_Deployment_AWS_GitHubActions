7889be1a22c5f71b8a97c45f7d93c5f9e88c2af6
pgopasina <pgopasina@miraclesoft.com>
2024-04-24 20:39:37 +0530

    updated msg in index.js file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hello World wellcome to GitHub actions" }');
 });
 
 app.get('/ready', function (req, res) {
